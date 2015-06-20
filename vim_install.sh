317599b009b537528c5981c911b6498ff370fcad
布鲁 <zhangqin1@ganji.com>
2015-06-20 17:05:13 +0800

    change vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-echo "install vundle"
+echo "+++++++++++++++++++install vundle+++++++++++++++++++"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "install color molokai"
+echo "+++++++++++++++++++install color molokai+++++++++++++++++++"
 git clone https://github.com/tomasr/molokai.git
-mv molokai/colors ~/.vim
-echo "install vim config .vimrc"
-mv .vimrc ~/
+cp -rf molokai/colors ~/.vim
+echo "+++++++++++++++++++install vim config .vimrc+++++++++++++++++++"
+cp -rf .vimrc ~/
+rm -rf mokokai
+echo "+++++++++++++++++++use vim enter :BundleInstall+++++++++++++++++++"
 
